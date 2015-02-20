9cdbc0666f3c1b1a31b6b5523b16bc56117f163d
ukidabek <ukidabek@gmail.com>
2015-02-20 20:54:23 +0100

    Zmiany w paralaksie.

---
+++ Assets/Scripts/Environment/CloudController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour 
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {  
+        if(coll.tag == "Parallax")
+           Destroy(this.gameObject);
+    }
+
+}
--- Assets/Scripts/Environment/ParallaxController.cs
+++ Assets/Scripts/Environment/ParallaxController.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ParallaxController : MonoBehaviour 
 {
     public GameObject skybox;
     public Sprite skyboxSprite;
     public float skyboxSpeed = 0.01f;
+
+    public GameObject cloud;
+    private List<Object> cloudsObjects = new List<Object>();
+    private SpriteRenderer cloudsRenderer;
+    public Sprite[] clouds;
+
+    public float cloudsSpawnTime = 300f;
+    private float cloudsSpawnTimer;
+    public float cloudsMinSpeed = 10f;
+    public float cloudsMaxSpeed = 20f;
 
     public GameObject[] parallaxLayers;
     public float[] parallaxLayerSpeed;
@@ -18,6 +29,8 @@ public class ParallaxController : MonoBehaviour
     {
         _playerInputs = GetComponentInParent<CameraTracking>().player.GetComponent<Inputs>();
         skybox.GetComponent<SpriteRenderer>().sprite = skyboxSprite;
+        cloudsSpawnTimer = cloudsSpawnTime;
+        cloudsRenderer = cloud.GetComponent<SpriteRenderer>();
     }
     
     void Update () 
@@ -43,6 +56,26 @@ public class ParallaxController : MonoBehaviour
                                                                         parallaxLayers[i].transform.localPosition.y, 
                                                                         parallaxLayers[i].transform.localPosition.z);
             }
+        }
+
+        if (cloudsSpawnTimer <= 0 && cloudsObjects.Count <= 10)
+        {
+            cloudsSpawnTimer = cloudsSpawnTime;
+            cloudsRenderer.sprite = clouds[Random.Range(0, clouds.Length)];
+            Object o = Instantiate(cloud, new Vector3(20, Random.Range(5,1) ,0),
+                                   Quaternion.identity);
+
+            (o as GameObject).GetComponent<Rigidbody2D>().velocity = new Vector2(
+                -Random.Range(cloudsMinSpeed, cloudsMaxSpeed)
+                , 0
+                );
+            (o as GameObject).GetComponent<Transform>().parent = transform;
+
+            cloudsObjects.Add(o);
+
+        } else
+        {
+            cloudsSpawnTimer -= Time.deltaTime;
         }
 
     }

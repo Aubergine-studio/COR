f84dab7d5811c43f899a04010084c30f170e25d3
ukidabek <ukidabek@gmail.com>
2015-03-01 21:51:13 +0100

    Dodanie NPC Lena siostra Khana.

---
+++ Assets/Scripts/Characters/NPC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : Character
+{
+	protected override void Actions ()
+	{
+		inputs.isGrounded = true;
+	}
+
+	void Update()
+	{
+		Actions ();
+	}
+}

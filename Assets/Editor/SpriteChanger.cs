f097359bd80dd885f1277cd162cc3caa1cfc4c97
ukidabek <ukidabek@gmail.com>
2015-05-03 00:02:26 +0200

    Podmiana fillerów

---
+++ Assets/Editor/SpriteChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SpriteChanger : EditorWindow
+{
+    private Sprite newSprite;
+
+    [MenuItem("Window/SpriteChanger")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(SpriteChanger));
+    }
+
+    void OnGUI()
+    {
+        GameObject[] tabGameObject = Selection.gameObjects;
+        newSprite = EditorGUILayout.ObjectField("New sprite: ", newSprite, typeof(Sprite), true) as Sprite;
+        if (GUILayout.Button("Change!") && newSprite != null)
+            foreach (GameObject obj in tabGameObject)
+            {
+                obj.GetComponent<SpriteRenderer>().sprite = newSprite;
+            }
+    }
+}
